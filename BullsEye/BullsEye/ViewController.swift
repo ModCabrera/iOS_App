8a728de889cb4e995e95611b2fdc9a232831ef51
Modesto Cabrera <cabrera.modesto@yahoo.com>
2017-12-12 20:27:48 +0200

    halfway

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    var targetValue: Int = 0
+    @IBOutlet weak var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(currentValue)")
+    }
+    
+    @IBAction func showAlert() {
+        currentValue = lroundf(slider.value)
+        let message = "The value of the slider: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        startNewRound()
+    }
 }
-
